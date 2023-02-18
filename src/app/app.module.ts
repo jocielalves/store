46cdbabbbec45a0164e1f24f509192c126abf2f9
jociel <jocielalvesdejesus@gmail.com>
2023-02-18 19:43:57 -0300

    Criação do primeiro projeto com angular seguindo curso da alura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    
+    imports: [
+        BrowserModule,
+        PhotosModule,
+        AppRoutingModule,
+        ErrorsModule
+      ],
+    
+    providers: [],
+    
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/photos/photo/photo.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+    selector: 'st-photo',
+    templateUrl: './photo.component.html'
+
+})
+
+
+export class PhotoComponent{
+
+    @Input()  description = '';
+    @Input() url ='';
+
+
+}
