24bb5837193fd310d9d314d5302b896b560cb942
jociel <jocielalvesdejesus@gmail.com>
2023-02-19 15:11:32 -0300

    adicionando componente button

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/Photo';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -8,23 +10,49 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos : Photo[] = [];
+  filter : string = '';
+  debounce: Subject<any> = new Subject<any>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
  
-  constructor( 
-    private photoService : PhotoService,
-    private activatedRoute : ActivatedRoute
+  constructor(
+    private activatedRoute : ActivatedRoute,
+    private photoService : PhotoService
     ){}
-
-
+  
+  onKeyUp(target : any) {
+    if(target instanceof EventTarget) {
+      var elemento = target as HTMLInputElement;
+      this.filter = elemento.value;
+    }
+  }
+  
   ngOnInit(): void {
 
-    const userName =  this.activatedRoute.snapshot.params['userName'];
-    this.photoService.listFromUser(userName)
-    .subscribe( photos => this.photos = photos)
-    
+    this.userName = this.activatedRoute.snapshot.params['userName'];
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce.pipe(debounceTime(300))
+    .subscribe(filter => this.filter  = filter);
   }  
-    
+  
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
 
+  load(){
+    this.photoService
+    .listFromUserPaginated(this.userName, ++this.currentPage)
+    .subscribe(photos => {
+      this.photos = this.photos.concat(photos);
+    
+    if(!photos.length) this.hasMore = false;
+
+    });
+  }
+  
 }
